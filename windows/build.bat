46063cdee02bd1e47d186c051652513bc4dfb965
Leonardo Bloise <leonardocsbloise@gmail.com>
2025-05-10 12:33:29 -0300

    fix: Windows build

--- windows/build.bat
+++ windows/build.bat
@@ -1,6 +1,3 @@
 @echo off
-REM Clean and build the shadow JAR using Gradle
 gradlew clean shadowJar
-
-REM Generate native image using GraalVM
 native-image -H:IncludeResources=".*\\.cow" -jar app\build\libs\cowsay.jar
